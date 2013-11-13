6fa1c8ae500cd2c06f149042c9fb6005088604e5
Kevin Jenkins <kevin@Kevins-MacBook-Pro.local>
2013-11-13 16:15:34 -0500

    Initial Commit

---
+++ MobileShopLocator/MobileShopLocator.h
@@ -0,0 +1,13 @@
+//
+//  MobileShopLocator.h
+//  MobileShopLocator
+//
+//  Created by Kevin Jenkins on 11/13/13.
+//  Copyright (c) 2013 somethingPointless. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MobileShopLocator : NSObject
+
+@end
---
+++ MobileShopLocator/MobileShopLocator.m
@@ -0,0 +1,13 @@
+//
+//  MobileShopLocator.m
+//  MobileShopLocator
+//
+//  Created by Kevin Jenkins on 11/13/13.
+//  Copyright (c) 2013 somethingPointless. All rights reserved.
+//
+
+#import "MobileShopLocator.h"
+
+@implementation MobileShopLocator
+
+@end
